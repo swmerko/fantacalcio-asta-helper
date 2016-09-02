f96b123873f4c8e5f0e26672044f908c21cdfa16
Erko <matteo.ercolani@gmail.com>
2016-09-02 18:30:56 +0200

    aggiornamento dati giocatori al 2 settembre 2016

--- players/import_script.py
+++ players/import_script.py
@@ -1,9 +1,5 @@
-from players.data.data_2016 import DATA
+from players.data.data_20160802 import DATA
 from players.models import Player
 
 for p in DATA:
     Player.objects.create(**p)
-    try:
-        Player.objects.create(**p)
-    except Exception as exc:
-        print p['name']
